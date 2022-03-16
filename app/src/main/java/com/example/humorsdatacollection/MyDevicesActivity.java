c9a8a1263cfa8742bccb5d24cd96eff1e41b0898
sagarhosur814 <58627154+sagarhosur814@users.noreply.github.com>
2022-03-16 19:20:55 +0530

    date recive has to recode again

---
+++ app/src/main/java/com/example/humorsdatacollection/MyDevicesActivity.java
@@ -0,0 +1,14 @@
+package com.example.humorsdatacollection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyDevicesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mydevices);
+    }
+}
